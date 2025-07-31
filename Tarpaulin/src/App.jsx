065c38f9dbacbc661c0b2e9101c98ca33c135aad
TamilNangaiK <k.tamilpirai@gmail.com>
2025-08-01 02:54:43 +0530

    Initial commit of Vite project

---
+++ Tarpaulin/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Contactus from './Contactus'
+import Hero from './Hero'
+import image from './assets/contactus/Contactus.png'
+function App() {
+  
+  return (
+    <>
+      {/* <Hero/> */}
+      <Contactus heroimage={image}/>
+    </>
+  )
+}
+
+export default App
+
---
+++ Tarpaulin/src/Contactus.jsx
@@ -0,0 +1,9 @@
+const Contactus=({heroimage}) =>{
+  return (
+    <>       
+        <img  className='border-4 border-solid-black  max-lg:w-1440px max-lg:h-539px max-lg:t-62px max-sm:w-1027px max-sm:h-434px max-sm:t-75px  w-362px h-160px t-92px' src={heroimage} alt="heroimage" />
+    </>
+  )
+}
+
+export default Contactus
